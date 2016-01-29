2e662a91fa3f806e2a1492616da40f4f21e305fa
dcai <dcai@financialengines.com>
2016-01-28 21:01:40 -0800

    added Person interface

--- app/pages/home/home.ts
+++ app/pages/home/home.ts
@@ -1,13 +1,14 @@
 import {Page, NavController} from 'ionic-framework/ionic';
-import {PersonPage} from '../person/person';
+import {Person, PersonPage} from '../person/person';
 import {PersonService} from '../person/person.service';
+
 
 @Page({
   templateUrl: 'build/pages/home/home.html',
 })
 export class HomePage {
 
-  people: Array<{ name: string, status: string }>;
+  people: Array<Person>;
 
   constructor(private nav: NavController, PersonService: PersonService) {
     this.people = PersonService.getPeople();
--- app/pages/person/person.ts
+++ app/pages/person/person.ts
@@ -2,13 +2,20 @@ import {Page, NavController, NavParams} from 'ionic-framework/ionic';
 // import * as _ from 'lodash';
 import {PersonService} from './person.service';
 
+
+export interface Person {
+  name: string;
+  status: string;
+}
+
+
 @Page({
   templateUrl: 'build/pages/person/person.html'
 })
 export class PersonPage {
-  person: any;
+  person: Person;
 
   constructor(private nav: NavController, navParams: NavParams) {
-    this.person = navParams.get('person');
+    this.person = navParams.get('person') || {};
   }
 }
